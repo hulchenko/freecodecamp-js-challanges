439692f0ee3ea0cd6e6a8e29cbb1af1df7b7c9af
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-02-25 11:34:47 -0700

    add 8th challenge

--- script.js
+++ script.js
@@ -72,7 +72,18 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
-///////////////////////////////////////#8
+///////////////////////////////////////#8 Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+function truncateString(str, num) {
+  let string = "";
+ if (str.slice(0, num) == str) {
+   return str;
+ } else {
+   let short = str.slice(0,num) + "...";
+   return short;
+ }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
 ///////////////////////////////////////#9
 ///////////////////////////////////////#10
 ///////////////////////////////////////#11
