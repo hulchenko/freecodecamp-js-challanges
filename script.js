5d56679a43e3e8b33f4a90b7eac3e0e2aaf7da87
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-02-25 10:21:22 -0700

    add 4th challenge

--- script.js
+++ script.js
@@ -13,22 +13,31 @@ function reverseString(str) {
   }
   
   reverseString("hello");
-/////////////////////////////////////// #3 Find the Longest Word in a String
+/////////////////////////////////////// #3 Factorialize a Number
+function factorialize(num) {
+  for (var result = 1; num > 0; num--) {
+    result *= num;
+  }
+  return result;
+}
+
+factorialize(5);
+
+///////////////////////////////////////#4 Find the Longest Word in a String
 function findLongestWordLength(str) {
-    var arr = str.split(" ");//split sentence to an array
-     var longestWord = arr[0];//assume longest word is the first word of an array
-     for (var i=0; i< arr.length; i++){ //iterate through array of words
-       
-       if(arr[i].length > longestWord.length){
-         //compare other words of an array if they are longer than the first one
-         longestWord = arr[i];
-       }
-     }
-     return longestWord.length;//return length of the longest word
+  var arr = str.split(" ");//split sentence to an array
+   var longestWord = arr[0];//assume longest word is the first word of an array
+   for (var i=0; i< arr.length; i++){ //iterate through array of words
      
+     if(arr[i].length > longestWord.length){
+       //compare other words of an array if they are longer than the first one
+       longestWord = arr[i];
+     }
    }
-   findLongestWordLength("The quick brown fox jumped over the lazy dog");
-///////////////////////////////////////#4
+   return longestWord.length;//return length of the longest word
+   
+ }
+ findLongestWordLength("The quick brown fox jumped over the lazy dog");
 ///////////////////////////////////////#5
 ///////////////////////////////////////#6
 ///////////////////////////////////////#7
