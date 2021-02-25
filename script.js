8c499333cbe9b0dd37ba0ed3b264b9b011b10337
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-02-25 10:44:08 -0700

    add 4th challenge

--- script.js
+++ script.js
@@ -38,7 +38,17 @@ function findLongestWordLength(str) {
    
  }
  findLongestWordLength("The quick brown fox jumped over the lazy dog");
-///////////////////////////////////////#5
+///////////////////////////////////////#5 Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(Math.max(...arr[i]));{
+    }
+  }
+  return newArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 ///////////////////////////////////////#6
 ///////////////////////////////////////#7
 ///////////////////////////////////////#8
