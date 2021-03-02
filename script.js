57ad23a7ed52be382316e5f1ff083a9ac46a7e2e
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-03-02 12:36:06 -0700

    add revised solution to #9 Challenge

--- script.js
+++ script.js
@@ -90,6 +90,19 @@ function findElement(arr, func) {
 }
 
 findElement([1, 2, 3, 4], num => num % 2 === 0);
+//OR//
+function findElement(arr, func) {
+  let num = 0;
+for (let i = 0; i < arr.length; i++){
+    num = arr[i];
+    if(func(num)){
+    return num;
+    }      
+}  
+ return undefined;
+}
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
 ///////////////////////////////////////#10 Check if a value is classified as a boolean primitive. Return true or false.
 function booWho(bool) {
   let test = typeof(bool);
