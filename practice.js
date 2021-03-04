14c7a21a537e3d2bfaee0967705ccee9ed3c2262
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-03-03 20:03:58 -0700

    add lesson

--- practice.js
+++ practice.js
@@ -8,13 +8,57 @@
 //   })
 // }
 
-document.getElementById("wrapper").addEventListener("click",function(event){
-    var tagName = event.target.tagName.toLowerCase();
-    if (tagName === "p" || tagName === "li" ) {
-      event.target.style.color = "blue";
-    }
+// document.getElementById("wrapper").addEventListener("click",function(event){
+//     var tagName = event.target.tagName.toLowerCase();
+//     if (tagName === "p" || tagName === "li" ) {
+//       event.target.style.color = "blue";
+//     }
     
-    if (event.target.classList.contains('color')){
-        event.target.style.color = "red";
-        }
-  })
+//     if (event.target.classList.contains('color')){
+//         event.target.style.color = "red";
+//         }
+//   })
+
+// document.querySelector('#alert').addEventListener("click",function(){
+//   alert("Hello!")
+// })
+
+// document.querySelector('#confirm').addEventListener("click",function(){
+//   var decision = confirm("Are you sure?")
+//   if (decision === true) {
+//     alert("It's true!")
+//   } else {
+//     alert("It's false!")
+//   }
+// })
+
+// document.querySelector('#prompt').addEventListener("click",function(){
+//   console.log(prompt("What is your Name?"));
+// })
+
+// var string = "1,2,3,4,5,6,7";
+// var arrayOfStrings = string.split(",");
+// var num = arrayOfStrings.map(function(i){
+//   return i * 2
+// });
+// console.log(num);
+
+// var num = 2.01;
+
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.random()*2)
+
+// var person = {
+//   name: "Max",
+//   age: 18,
+//   car: {
+//     model: "Ford"
+//   },
+//   job: "FrontEnd",
+//   friends: ["Vadik", "Bailey"] 
+// }
+
+
+// var toString = JSON.stringify(person);
+// console.log(JSON.parse(toString));
