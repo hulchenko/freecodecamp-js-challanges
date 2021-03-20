188b98694f5e5437e02de405740df2be2ae20367
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-03-20 13:19:35 -0600

    add tasks

--- fcc-practice.js
+++ fcc-practice.js
@@ -1,8 +1,10 @@
 //Object:
 let bird = {
     name = 'Crow',
-    numLegs = 2;
+    numLegs = 2
 }
+
+
 
 //Constructor:
 function Bird(name) {
@@ -12,9 +14,63 @@ function Bird(name) {
 
 let canary = new Bird('Tweety'); //object created from constructor
 let ownProps = [];
-for (let property in canary) {
+
+
+
+
+//check for all properties within the object
+for (let property in canary) { 
   if (canary.hasOwnProperty(property)) {
     ownProps.push(property);
   }
 }
 console.log(ownProps); //[ 'name', 'numLegs' ]
+
+
+
+//Constructor + prototype:
+function Dog(name) {
+    this.name = name;
+  }
+  Dog.prototype.numLegs = 4; //to avoid duplicated variables. All objects created from this constructor will have numLegs = 4;
+
+
+
+
+//check for all properties and prototype properties within the object
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+    
+  for (let property in beagle){
+    if(beagle.hasOwnProperty(property)){
+      ownProps.push(property)
+    } else {
+      prototypeProps.push(property)
+    }
+  }
+
+  //Construction property:
+let duck = new Bird();
+let beagle = new Dog();
+
+console.log(duck.constructor === Bird); 
+console.log(beagle.constructor === Dog);
+//This can be useful with:
+function Dog(name) {
+    this.name = name;
+  }
+  function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog){
+    return true;
+  } else {
+    return false;
+  }
+  }
